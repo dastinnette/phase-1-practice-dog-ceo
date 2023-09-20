93e5b332b7b21dcae9988c443b14e84114713b0d
David Stinnette <dastinnette@gmail.com>
2023-09-20 17:25:16 -0400

    Rendering images to the DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const dogImgContainer = document.getElementById('dog-image-container')
 
+fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(dogData => {
+        dogData.message.forEach(renderDog)
+    })
+
+function renderDog(dogObj) {
+    const img = document.createElement('img')
+    img.src = dogObj
+
+    dogImgContainer.append(img)
+}
